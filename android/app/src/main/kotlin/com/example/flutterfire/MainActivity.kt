e2284b6072e93ea43c0ce1d74b59cccb78ec859c
Your Name <you@example.com>
2022-10-24 10:58:22 +0700

    Flutter fire introduction

---
+++ android/app/src/main/kotlin/com/example/flutterfire/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutterfire
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text("Dashboard"),
+          actions: const [],
+        ),
+        body: const Center(
+          child: Text("Dashboard"),
+        ));
+  }
+}
